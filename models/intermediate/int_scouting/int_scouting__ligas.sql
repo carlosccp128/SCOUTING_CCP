cb8711829996ec5045a9bd6c8cc2c44288094762
Carlos Ceballos <ccpcarlos128@gmail.com>
2026-05-17 23:09:25 +0000

    int_scouting__ligas.sql  hecho

--- models/intermediate/int_scouting/int_scouting__ligas.sql
+++ models/intermediate/int_scouting/int_scouting__ligas.sql
@@ -1,5 +1,51 @@
 -- int_scouting__ligas.sql
 --=========================
-with stg_paises as (
+with stg_ligas as (
     select * from {{ ref('stg_scouting__ligas') }}
 ),
+
+stg_paises as (
+    select * from {{ ref('stg_scouting__paises') }}
+),
+
+stg_list as (
+    select * from {{ ref('stg_scouting__listado_jugadores') }}
+),
+list_liga as (
+    select distinct
+        id_liga,
+        nombre_liga,
+        division_liga
+    from stg_list
+),
+list_pais as (
+    select distinct
+        id_pais,
+        nombre_pais
+    from stg_list
+),
+
+pais_ident as (
+    select 
+        a.*,
+        b.id_pais
+    from stg_paises as a
+    left join list_pais as b
+        on a.nombre_pais = b.nombre_pais
+)
+
+
+
+select
+    l.id_liga,
+    l.nombre_liga,
+    j.division_liga,
+    p.id_pais
+
+from stg_ligas as l
+left join list_liga as j
+    on l.id_liga = j.id_liga
+left join pais_ident as p
+    on l.id_pais_liga = p.id_pais_liga
+
+
