4641e3d249a396f81decf64492643ce40172ceeb
Carlos Ceballos <ccpcarlos128@gmail.com>
2026-05-20 02:31:10 +0000

    mejor filtrado en int_scouting_atributos jugadores

--- models/intermediate/int_scouting/int_scouting__atributos_jugadores.sql
+++ models/intermediate/int_scouting/int_scouting__atributos_jugadores.sql
@@ -3,6 +3,15 @@
 
 with stg_atributos_jug as (
     select * from {{ ref('stg_scouting__atributos_jugadores') }}
+),
+
+int_jug as (
+    select * from {{ ref('int_scouting__jugadores') }}
 )
 
-select * from stg_atributos_jug
+
+select 
+    a.*
+from stg_atributos_jug as a
+inner join int_jug as j
+    on a.id_jugador = j.id_jugador
